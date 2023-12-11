154edf8bce94319017729b96a323a7cc5ddbc843
Isaac Vicente <isaac.silva@lsd.ufcg.edu.br>
2023-12-11 15:28:53 -0300

    feat: use `curl` instead of `wget`

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@ apt purge cups
 apt install cups-client
 
 ## Copy this file to /etc/cups
-wget -P /etc/cups/ https://raw.githubusercontent.com/ufcg-lsd/configure-cups/main/client.conf
+curl -o /etc/cups/client.conf https://raw.githubusercontent.com/ufcg-lsd/configure-cups/main/client.conf
